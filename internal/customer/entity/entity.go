daa84029cb58cc6f6951c40bda30cdf4d1c4d22c
gabrielalmir <gabr.almir@gmail.com>
2024-02-25 19:14:08 -0300

    feat: index basic fields in entities

--- internal/customer/entity/entity.go
+++ internal/customer/entity/entity.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type Customer struct {
-	ID           uint64        `gorm:"primaryKey;autoIncrement;column:id"`
+	ID           uint64        `gorm:"primaryKey;autoIncrement;column:id;index"`
 	Limit        int64         `gorm:"column:limite;not null"`
 	Balance      int64         `gorm:"column:saldo;not null"`
 	Transactions []Transaction `gorm:"foreignKey:CustomerID"`
@@ -21,12 +21,12 @@ func NewCustomer(limit, balance int64) *Customer {
 }
 
 type Transaction struct {
-	ID          uint64    `gorm:"primaryKey;autoIncrement;column:id"`
+	ID          uint64    `gorm:"primaryKey;autoIncrement;column:id;index"`
 	Amount      int64     `gorm:"column:valor;not null"`
 	Type        string    `gorm:"column:tipo;not null"`
 	Description string    `gorm:"column:descricao;not null"`
 	CreatedAt   time.Time `gorm:"column:criado_em;not null"`
-	CustomerID  uint64    `gorm:"column:cliente_id;not null"`
+	CustomerID  uint64    `gorm:"column:cliente_id;not null;index"`
 }
 
 func (t *Transaction) TableName() string {
