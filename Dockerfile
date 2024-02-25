aeba9b8bd1cc83b8f184642b5776779eccd3d172
gabrielalmir <gabr.almir@gmail.com>
2024-02-24 21:40:37 -0300

    feat: add hello world route

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
+## Build
 FROM golang:1.21.5 AS build
 WORKDIR /app
+
 COPY . .
+
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./cmd/api
 
+## Deploy
 FROM scratch
+
 COPY --from=build /app/api /app/api
+
+ENV GIN_MODE=release
+
 ENTRYPOINT [ "/app/api" ]
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "Hello, world!"})
+	})
+
+	println("Server is running on port 8080")
+	log.Fatalln(r.Run(":8080"))
 }
