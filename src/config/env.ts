236ef50d750338d63909a4908dcf4c547aa777c7
gabrielalmir <gabr.almir@gmail.com>
2024-05-11 19:16:52 -0300

    feat: add migrations for prisma

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,13 +3,9 @@ import zod from 'zod'
 
 // Define environment schema
 const envSchema = zod.object({
-    NODE_ENV: zod.string().default('development'),
+    NODE_ENV: zod.string().default('production'),
     PORT: zod.number().default(3000),
-    DB_HOST: zod.string().default('localhost'),
-    DB_PORT: zod.string().default('5432'),
-    DB_USER: zod.string().default('postgres'),
-    DB_PASS: zod.string().default('postgres'),
-    DB_NAME: zod.string().default('postgres'),
+    DATABASE_URL: zod.string(),
 })
 
 export type Env = zod.infer<typeof envSchema>
